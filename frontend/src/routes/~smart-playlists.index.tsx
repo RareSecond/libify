2a7adbf94f9cae3efd52b87ec6c85b2d26088f50
RareSecond <jasper@codictive.be>
2025-12-16 09:20:25 +0100

    Restyle to the Smart Playlists page

--- frontend/src/routes/~smart-playlists.index.tsx
+++ frontend/src/routes/~smart-playlists.index.tsx
@@ -1,3 +1,4 @@
+import { Container } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { PageTitle } from "../components/PageTitle";
@@ -9,9 +10,9 @@ export const Route = createFileRoute("/smart-playlists/")({
 
 function SmartPlaylistsIndexPage() {
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <Container className="py-8" fluid>
       <PageTitle title="Smart Playlists" />
       <SmartPlaylists />
-    </div>
+    </Container>
   );
 }
